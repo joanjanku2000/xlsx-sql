2acca696b5766d5547863d1be32f37d480b50b18
Joan Janku <janku.joan4@gmail.com>
2023-10-29 21:37:41 +0100

    base common methods for adaptions

--- src/main/java/service/generator/SqlGenerator.java
+++ src/main/java/service/generator/SqlGenerator.java
@@ -1,9 +1,13 @@
 package service.generator;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.stream;
 import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.StringUtils.substringAfter;
+import static org.apache.commons.lang3.StringUtils.substringBefore;
 
 public interface SqlGenerator {
     Integer COLUMN_NAMES_INDEX = 0;
@@ -22,6 +26,9 @@ public interface SqlGenerator {
     String SEMICOLON = ";";
     String EQUALZ = "=";
     String ASTERISK = "*"; // the column which has this will be used in the predicate
+    String GREATER_THAN = ">";
+    String QUESTION_MARK = "?";
+    String AMPERSAND = "&";
 
     String generate(String tableName, Map<Integer, List<String>> rowsMap);
 
@@ -32,5 +39,22 @@ public interface SqlGenerator {
 
     default List<String> extractColumnNames(Map<Integer, List<String>> rowsMap) {
         return requireNonNull(rowsMap.get(COLUMN_NAMES_INDEX));
+    }
+
+    default boolean columnNeedsAdapting(String columnName){
+        return columnName.contains(GREATER_THAN);
+    }
+    default Map<String ,String> extractColumnTranslation(String adaptedColumnName){
+        Map<String,String> toReturn = new HashMap<>();
+        String adaptions = substringAfter(adaptedColumnName,GREATER_THAN);
+        String[] parts = adaptions.split(AMPERSAND);
+        stream(parts).forEach(s -> toReturn.put(s.split(EQUALZ)[0],s.split(EQUALZ)[1]));
+        return toReturn;
+    }
+    default String pureColumnName(String adaptedColumnName){
+        return substringBefore(adaptedColumnName,GREATER_THAN);
+    }
+    default boolean ignoreColumn(String columnName){
+        return columnName.contains(OPENING_BRACKET) && columnName.contains(CLOSING_BRACKET);
     }
 }
