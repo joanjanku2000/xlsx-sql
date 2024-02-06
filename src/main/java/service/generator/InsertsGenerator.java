bfd90a7c8256bf85faaff660cee877af021a088c
joanjanku2000 <janku.joan4@gmail.com>
2024-02-06 21:11:08 +0100

    Inserts Generator: add ignored column support

--- src/main/java/service/generator/InsertsGenerator.java
+++ src/main/java/service/generator/InsertsGenerator.java
@@ -16,10 +16,17 @@ public class InsertsGenerator implements SqlGenerator {
         StringBuilder values = new StringBuilder();
         for (Map.Entry<Integer, List<String>> e : rowsMap.entrySet()) {
             List<String> valuesList = e.getValue();
-            String value = OPENING_BRACKET + valuesList.stream().map(val -> getValue(columnsMap.get(valuesList.indexOf(val)), val)).collect(Collectors.joining(COMMA)) + CLOSING_BRACKET;
+            String value = OPENING_BRACKET + valuesList.stream()
+                    .filter(val -> !columnsMap.get(valuesList.indexOf(val)).contains(ASTERISK))
+                    .map(val -> getValue(columnsMap.get(valuesList.indexOf(val)), val))
+                    .collect(Collectors.joining(COMMA)) + CLOSING_BRACKET;
             values.append(COMMA).append(value);
         }
-        return INSERT_STATEMENT.replace(TABLE_NAME, tableName).replace(COLUMN_NAMES, columnsMap.values().stream().map(this::getColumnName).filter(Objects::nonNull).collect(Collectors.joining(COMMA))).replace(VALUES, values.toString().replaceFirst(COMMA, EMPTY));
+        return INSERT_STATEMENT.replace(TABLE_NAME, tableName).replace(COLUMN_NAMES, columnsMap.values().stream().filter(c -> !c.contains(ASTERISK)).collect(Collectors.toList())
+                .stream().map(this::getColumnName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(COMMA)))
+                .replace(VALUES, values.toString().replaceFirst(COMMA, EMPTY));
     }
 
 
