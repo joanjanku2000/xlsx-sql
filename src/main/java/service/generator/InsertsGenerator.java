604729bed32ccec0375a97045576fb3a6e03b840
joanjanku2000 <janku.joan4@gmail.com>
2024-02-06 21:15:02 +0100

    Inserts Generator: roll it back since it is already found in getValue method

--- src/main/java/service/generator/InsertsGenerator.java
+++ src/main/java/service/generator/InsertsGenerator.java
@@ -17,12 +17,11 @@ public class InsertsGenerator implements SqlGenerator {
         for (Map.Entry<Integer, List<String>> e : rowsMap.entrySet()) {
             List<String> valuesList = e.getValue();
             String value = OPENING_BRACKET + valuesList.stream()
-                    .filter(val -> !columnsMap.get(valuesList.indexOf(val)).contains(ASTERISK))
                     .map(val -> getValue(columnsMap.get(valuesList.indexOf(val)), val))
                     .collect(Collectors.joining(COMMA)) + CLOSING_BRACKET;
             values.append(COMMA).append(value);
         }
-        return INSERT_STATEMENT.replace(TABLE_NAME, tableName).replace(COLUMN_NAMES, columnsMap.values().stream().filter(c -> !c.contains(ASTERISK)).collect(Collectors.toList())
+        return INSERT_STATEMENT.replace(TABLE_NAME, tableName).replace(COLUMN_NAMES, columnsMap.values()
                 .stream().map(this::getColumnName)
                 .filter(Objects::nonNull)
                 .collect(Collectors.joining(COMMA)))
